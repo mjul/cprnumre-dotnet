ca9e249acf88cd56e0ec54e84f26f6e58bfe1a29
Martin Jul <no-spam@mjul.com>
2020-01-27 12:24:09 +0100

    Add basic Fake build script

---
+++ build.fsx
@@ -0,0 +1,49 @@
+#r "paket:
+nuget Fake.DotNet.Cli
+nuget Fake.IO.FileSystem
+nuget Fake.Core.Target //"
+#load ".fake/build.fsx/intellisense.fsx"
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+
+Target.initEnvironment ()
+
+let srcDir = "src"
+let testDir = "tests"
+
+Target.create "Clean" (fun _ ->
+    !! (srcDir @@ "**/bin")
+    ++ (srcDir @@ "**/obj")
+    ++ (testDir @@ "**/bin")
+    ++ (testDir @@ "**/obj")
+    |> Shell.cleanDirs 
+)
+
+Target.create "BuildLibrary" (fun _ ->
+    !! (srcDir @@ "**/*.*proj")
+    |> Seq.iter (DotNet.build id)
+)
+
+Target.create "BuildTests" (fun _ ->
+    !! (testDir @@ "**/*.*proj")
+    |> Seq.iter (DotNet.build id)
+)
+
+Target.create "Test" (fun _ ->
+    !! (testDir @@ "**/*.fsproj")
+    |> Seq.iter (DotNet.test id)
+)
+
+Target.create "All" ignore
+
+"Clean"
+  ==> "BuildLibrary"
+  ==> "BuildTests"
+  ==> "Test"
+  ==> "All"
+
+Target.runOrDefault "All"
